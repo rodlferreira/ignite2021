8e2cd537162df40aa2f9687f45ede05ddcc4c59f
rodlferreira <rodriggo.lferreira@icloud.com>
2021-05-05 01:11:43 -0300

    segundo dia do Ignite parte 2(noturno)

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,2 +1,32 @@
-/*! For license information please see bundle.js.LICENSE.txt */
-(()=>{"use strict";var t={418:t=>{var e=Object.getOwnPropertySymbols,r=Object.prototype.hasOwnProperty,o=Object.prototype.propertyIsEnumerable;function n(t){if(null==t)throw new TypeError("Object.assign cannot be called with null or undefined");return Object(t)}t.exports=function(){try{if(!Object.assign)return!1;var t=new String("abc");if(t[5]="de","5"===Object.getOwnPropertyNames(t)[0])return!1;for(var e={},r=0;r<10;r++)e["_"+String.fromCharCode(r)]=r;if("0123456789"!==Object.getOwnPropertyNames(e).map((function(t){return e[t]})).join(""))return!1;var o={};return"abcdefghijklmnopqrst".split("").forEach((function(t){o[t]=t})),"abcdefghijklmnopqrst"===Object.keys(Object.assign({},o)).join("")}catch(t){return!1}}()?Object.assign:function(t,a){for(var i,c,p=n(t),s=1;s<arguments.length;s++){for(var u in i=Object(arguments[s]))r.call(i,u)&&(p[u]=i[u]);if(e){c=e(i);for(var f=0;f<c.length;f++)o.call(i,c[f])&&(p[c[f]]=i[c[f]])}}return p}},408:(t,e,r)=>{var o=r(418);if("function"==typeof Symbol&&Symbol.for){var n=Symbol.for;n("react.element"),n("react.portal"),n("react.fragment"),n("react.strict_mode"),n("react.profiler"),n("react.provider"),n("react.context"),n("react.forward_ref"),n("react.suspense"),n("react.memo"),n("react.lazy")}"function"==typeof Symbol&&Symbol.iterator;function a(t){for(var e="https://reactjs.org/docs/error-decoder.html?invariant="+t,r=1;r<arguments.length;r++)e+="&args[]="+encodeURIComponent(arguments[r]);return"Minified React error #"+t+"; visit "+e+" for the full message or use the non-minified dev environment for full errors and additional helpful warnings."}var i={isMounted:function(){return!1},enqueueForceUpdate:function(){},enqueueReplaceState:function(){},enqueueSetState:function(){}},c={};function p(t,e,r){this.props=t,this.context=e,this.refs=c,this.updater=r||i}function s(){}function u(t,e,r){this.props=t,this.context=e,this.refs=c,this.updater=r||i}p.prototype.isReactComponent={},p.prototype.setState=function(t,e){if("object"!=typeof t&&"function"!=typeof t&&null!=t)throw Error(a(85));this.updater.enqueueSetState(this,t,e,"setState")},p.prototype.forceUpdate=function(t){this.updater.enqueueForceUpdate(this,t,"forceUpdate")},s.prototype=p.prototype;var f=u.prototype=new s;f.constructor=u,o(f,p.prototype),f.isPureReactComponent=!0;Object.prototype.hasOwnProperty},294:(t,e,r)=>{r(408)}},e={};!function r(o){var n=e[o];if(void 0!==n)return n.exports;var a=e[o]={exports:{}};return t[o](a,a.exports,r),a.exports}(294)})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.jsx":
+/*!***********************!*\
+  !*** ./src/index.jsx ***!
+  \***********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Cannot find module '@babel/preset-react,'\\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:636:15)\\n    at resolve (/home/rodriggolima/Documentos/Ignite/01-github-explorer/node_modules/v8-compile-cache/v8-compile-cache.js:164:23)\\n    at resolveStandardizedName (/home/rodriggolima/Documentos/Ignite/01-github-explorer/node_modules/@babel/core/lib/config/files/plugins.js:96:7)\\n    at resolvePreset (/home/rodriggolima/Documentos/Ignite/01-github-explorer/node_modules/@babel/core/lib/config/files/plugins.js:44:10)\\n    at loadPreset (/home/rodriggolima/Documentos/Ignite/01-github-explorer/node_modules/@babel/core/lib/config/files/plugins.js:63:20)\\n    at loadPreset.next (<anonymous>)\\n    at createDescriptor (/home/rodriggolima/Documentos/Ignite/01-github-explorer/node_modules/@babel/core/lib/config/config-descriptors.js:179:16)\\n    at createDescriptor.next (<anonymous>)\\n    at step (/home/rodriggolima/Documentos/Ignite/01-github-explorer/node_modules/gensync/index.js:261:32)\\n    at evaluateAsync (/home/rodriggolima/Documentos/Ignite/01-github-explorer/node_modules/gensync/index.js:291:5)\");\n\n//# sourceURL=webpack://01-github-explorer/./src/index.jsx?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.jsx"]();
+/******/ 	
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+    mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -11,12 +13,18 @@ module.exports = {
 
     },
 
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
+
     module: {
         rules: [
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
-                use: 'babel-loader'
+                use: 'babel-loader',
             }
         ],
     }
